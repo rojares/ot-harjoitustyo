122f069ce7da5c6171e97ba3eb688c545f67e907
Rami Ojares <rami.ojares@helsinki.fi>
2019-11-12 22:11:25 +0200

    testeja

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,44 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti!=null);
     }
+
+    @Test
+    public void saldoAlussaOikein() {
+        assertTrue(10 == kortti.saldo());
+    }
+
+    @Test
+    public void lataaminenKasvattaa() {
+        kortti.lataaRahaa(5);
+        assertTrue(15 == kortti.saldo());
+    }
+
+    @Test
+    public void ottaminenToimii() {
+        kortti.otaRahaa(5);
+        assertTrue(5 == kortti.saldo());
+    }
+
+    @Test
+    public void vaheneeOikeinJosTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertTrue(5 == kortti.saldo());
+    }
+
+    @Test
+    public void eiMuutuJosEiTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertTrue(10 == kortti.saldo());
+    }
+
+    @Test
+    public void trueJosRiittaaMuutenFalse() {
+        Maksukortti korttiA = new Maksukortti(10);
+        assertTrue(true == korttiA.otaRahaa(10));
+        Maksukortti korttiB = new Maksukortti(10);
+        assertTrue(false == korttiB.otaRahaa(11));
+    }
+
 }
